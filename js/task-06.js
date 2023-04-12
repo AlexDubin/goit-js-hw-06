f3164a8e6947d074c7f39af2cfc6cbc5589129d0
Oleksandr Dubin <102242242+AlexDubin@users.noreply.github.com>
2023-04-12 22:46:51 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,3 +1,21 @@
+// const input = document.querySelector("#validation-input");
+// const inputValueLength = input.getAttribute("data-length");
+
+// input.addEventListener("blur", onInputBlur);
+
+// function onInputBlur(event) {
+//   const currentInputValueLength = event.currentTarget.value.length;
+
+//   if (currentInputValueLength === Number(inputValueLength)) {
+//     input.classList.add("valid");
+//     input.classList.remove("invalid");
+//   } else {
+//     input.classList.add("invalid");
+//     input.classList.remove("valid");
+//   }
+// }
+
+
 const input = document.querySelector("#validation-input");
 const inputValueLength = input.getAttribute("data-length");
 
@@ -7,10 +25,14 @@ function onInputBlur(event) {
   const currentInputValueLength = event.currentTarget.value.length;
 
   if (currentInputValueLength === Number(inputValueLength)) {
-    input.classList.add("valid");
-    input.classList.remove("invalid");
+    addClasses(input, "valid", "invalid");
   } else {
-    input.classList.add("invalid");
-    input.classList.remove("valid");
+    addClasses(input, "invalid", "valid");
   }
 }
+
+function addClasses(element, classToAdd, classToRemove) {
+  element.classList.add(classToAdd);
+  element.classList.remove(classToRemove);
+}
+
